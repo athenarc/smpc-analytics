7cf22cd32e432912d0b534ec0fb841e58bc82f3d
Thanos Giannopoulos <thanosgn@gmail.com>
2018-03-12 12:52:49 +0200

    Added python script to generate SecreC data

---
+++ data_generator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import pandas as pd
+import numpy as np
+import sys
+import math
+
+DATASET = 'syndata_upload_and_scaling_tests/centricity_identified.csv'
+OUTPUT = 'data_input.sc'
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def main():
+    df=pd.read_csv(DATASET,sep=',')
+    A = np.array(df)
+    N = A.shape[0]
+    N = min(N,100)
+    M = A.shape[1]
+    with open(OUTPUT,'w') as output:
+        output.write('/* Rows: ' + str(N) + ' */' + '\n')
+        output.write('/* Attributes (' + str(M) + ') */' + '\n')
+        output.write('/**' + '\n')
+        for i in range(len(df.columns)):
+            col = df.columns[i]
+            output.write(' * '+ col + ' (' + str(i) + ')' + '\n')
+        output.write('**/' + '\n')
+        array = '{'
+        for i in range(N):
+            for j in range(M):
+                value = A[i,j]
+                if not is_number(value) or math.isnan(value):
+                    value = 0.0
+                array += str(value) + ','
+        array = array[:-1]
+        array += '}'
+        var = 'pd_shared3p float64[[2]] array'
+        command = 'reshape('+ array +', '+ str(N) +', '+ str(M) +');'
+        output.write(var + ' = ' + command + '\n')
+
+if __name__ == '__main__':
+    main()
