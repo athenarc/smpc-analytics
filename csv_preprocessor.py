4c4cec280fb42f11b836c306f3c3b71e638d03cb
Thanos Giannopoulos <thanosgn@gmail.com>
2018-03-27 18:10:02 +0300

    Split float values into buckets

--- csv_preprocessor.py
+++ csv_preprocessor.py
@@ -10,27 +10,30 @@ import csv
 if len(sys.argv) > 1:
     DATASET = sys.argv[1]
 else:
-    DATASET = 'syndata_upload_and_scaling_tests/centricity_identified.csv'
+    DATASET = 'syndata_upload_and_scaling_tests/centricity_identified_filtered.csv'
 
 DIRECTORY, BASENAME = os.path.split(DATASET)
 BASENAME = os.path.splitext(BASENAME)[0]
 OUTPUT = DIRECTORY + '/' + BASENAME + '_edited.csv'
-SERIALIZED = DIRECTORY + '/' + BASENAME + '_mapped_values.json'
+SERIALIZED = DIRECTORY + '/' + BASENAME + '_edited_mapped_values.json'
+CELLS = 4
+
 
 def main():
     print "Initializing ..."
     df=pd.read_csv(DATASET,sep=',')
     attribute_map = {}
+    attribute_min = {}
+    attribute_max = {}
     attribute_counter = {}
     rows = df.shape[0]
     string_attributes = []
     for attribute in df.columns:
-        column = df[attribute]
-        infered_type = str(column.dtype)
-        if infered_type == 'object':
-            attribute_map[attribute] = {}
-            attribute_counter[attribute] = 0
-            string_attributes.append(attribute)
+        attribute_map[attribute] = {}
+        attribute_counter[attribute] = 0
+        attribute_min[attribute] = min(df[attribute])
+        attribute_max[attribute] = max(df[attribute])
+
     print "Done initializing"
 
     print "Writing to csv ..."
@@ -38,16 +41,30 @@ def main():
         dataWriter = csv.writer(csv_edited, delimiter=',')
         dataWriter.writerow(df.columns.values)
         for i, row in df.iterrows():
-            for attribute in string_attributes:
+            for attribute in df.columns:
                 mapped_values = attribute_map[attribute]
                 value = row[attribute]
                 if value in mapped_values:
                     row[attribute] = mapped_values[value]
                 else:
-                    ai = attribute_counter[attribute]
-                    row[attribute] = ai
-                    mapped_values[value] = ai
-                    attribute_counter[attribute] += 1
+                    if str(df[attribute].dtype) == 'object':
+                        new_value = attribute_counter[attribute]
+                        attribute_counter[attribute] += 1
+                        mapped_values[value] = new_value
+                    else:
+                        minimum =  attribute_min[attribute]
+                        width = (attribute_max[attribute] - minimum) / CELLS
+                        new_value = int((value - minimum) / width)
+                        if new_value == CELLS:
+                            new_value -= 1
+                        start = minimum + new_value*width
+                        # print('[' + str(start) + ', ' + str(start+width) + ')')
+                        # print(new_value)
+                        # mapped_values[value] = new_value
+                        mapped_values['[' + str(start) + ', ' + str(start+width) + ')'] = new_value
+
+                    row[attribute] = new_value
+
             attribute_map[attribute] = mapped_values
             dataWriter.writerow(row)
 
