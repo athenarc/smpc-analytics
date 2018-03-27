37be5f8208a14d4bb54126b664e11c64e68841ff
Thanos Giannopoulos <thanosgn@gmail.com>
2018-03-27 12:47:11 +0300

    Mapped string attributes to consistent ai integers
    Co-authored-by: Dimitris Mouris <jimouris@gmail.com>

--- csv_preprocessor.py
+++ csv_preprocessor.py
@@ -4,6 +4,8 @@ import numpy as np
 import os.path
 import sys
 import math
+import json
+import csv
 
 if len(sys.argv) > 1:
     DATASET = sys.argv[1]
@@ -13,14 +15,46 @@ else:
 DIRECTORY, BASENAME = os.path.split(DATASET)
 BASENAME = os.path.splitext(BASENAME)[0]
 OUTPUT = DIRECTORY + '/' + BASENAME + '_edited.csv'
+SERIALIZED = DIRECTORY + '/' + BASENAME + '_mapped_values.json'
 
 def main():
+    print "Initializing ..."
     df=pd.read_csv(DATASET,sep=',')
+    attribute_map = {}
+    attribute_counter = {}
+    rows = df.shape[0]
+    string_attributes = []
     for attribute in df.columns:
-        infered_type = str(df[attribute].dtype)
+        column = df[attribute]
+        infered_type = str(column.dtype)
         if infered_type == 'object':
-            df[attribute] = 0
-    df.to_csv(OUTPUT, sep=',', index=False)
+            attribute_map[attribute] = {}
+            attribute_counter[attribute] = 0
+            string_attributes.append(attribute)
+    print "Done initializing"
+
+    print "Writing to csv ..."
+    with open(OUTPUT, 'w') as csv_edited:
+        dataWriter = csv.writer(csv_edited, delimiter=',')
+        dataWriter.writerow(df.columns.values)
+        for i, row in df.iterrows():
+            for attribute in string_attributes:
+                mapped_values = attribute_map[attribute]
+                value = row[attribute]
+                if value in mapped_values:
+                    row[attribute] = mapped_values[value]
+                else:
+                    ai = attribute_counter[attribute]
+                    row[attribute] = ai
+                    mapped_values[value] = ai
+                    attribute_counter[attribute] += 1
+            attribute_map[attribute] = mapped_values
+            dataWriter.writerow(row)
+
+    print "Serializing maps ..."
+    with open(SERIALIZED, 'w') as data_file:
+        j = json.dumps(attribute_map)
+        data_file.write(j)
 
 if __name__ == '__main__':
     main()
--- data_generator.py
+++ data_generator.py
@@ -7,17 +7,10 @@ import math
 if len(sys.argv) > 1:
     DATASET = sys.argv[1]
 else:
-    DATASET = 'syndata_upload_and_scaling_tests/centricity_identified.csv'
+    DATASET = 'syndata_upload_and_scaling_tests/centricity_identified_edited.csv'
 
 OUTPUT = 'data_input.sc'
 ROW_LIMIT = 1000
-
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
 
 def main():
     df=pd.read_csv(DATASET,sep=',')
@@ -36,37 +29,7 @@ def main():
         array = '{'
         for i in range(N):
             for j in range(M):
-                if not is_number(A[i,j]) or math.isnan(A[i,j]):
-                    A[i,j] = 0.0
                 array += str(A[i,j]) + ','
-        array = array[:-1]
-        array += '}'
-        var = 'pd_shared3p float64[[2]] imported_array'
-        command = 'reshape('+ array +', '+ str(N) +', '+ str(M) +');'
-        output.write(var + ' = ' + command + '\n')
-        mins = [str(min([A[i,j] for i in range(N)])) for j in range(M)]
-        # mins = [min(A[:,j]) for j in range(M)]
-        mins = '{' + ','.join(mins) + '}'
-        maxs = [str(max([A[i,j] for i in range(N)])) for j in range(M)]
-        maxs = '{' + ','.join(maxs) + '}'
-        # maxs = [max(A[:,j]) for j in range(M)]
-        output.write('pd_shared3p float64[[1]] imported_mins(' + str(M) + ') = ' + mins + ';\n')
-        output.write('pd_shared3p float64[[1]] imported_maxs(' + str(M) + ') = ' + maxs + ';\n')
-        # output.write('for (uint64 j = 0; j < ' + str(M) + '; j++ ) ;' + '\n')
-        # output.write('mins3[j] = min(array[:,j]);' + '\n')
-        # output.write('uint64 M = array_shape[1];' + '\n')
-        # output.write('uint64 M = array_shape[1];' + '\n')
-
-    # uint64[[1]] array_shape = shape(array); // array -> variable from data_input.sc
-    # uint64 N = array_shape[0];
-    # uint64 M = array_shape[1];
-    # pd_shared3p float64[[1]] mins3(M);
-    # pd_shared3p float64[[1]] maxs3(M);
-    # for (uint64 j = 0; j < M; j++ ) {
-    #   mins3[j] = min(array[:,j]);
-    #   maxs3[j] = max(array[:,j]);
-    # }
-
 
 
 if __name__ == '__main__':
