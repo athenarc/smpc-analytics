d626eed09d8f64ca33baa9a062bfe4c30136936c
Dimitris Mouris <jimouris@gmail.com>
2018-04-03 17:49:59 +0300

    Move javascript to different file, add css to move window to center

---
+++ web/frontend/logic.js
@@ -0,0 +1,24 @@
+var cb0 = document.querySelectorAll('input[type="checkbox"]')[0];
+var cb1 = document.querySelectorAll('input[type="checkbox"]')[1];
+var cb2 = document.querySelectorAll('input[type="checkbox"]')[2];
+
+cb0.addEventListener("click", function () {
+    if(cb0.checked)
+        document.getElementById("AgeCells").style.display = "block";
+    else
+        document.getElementById("AgeCells").style.display = "none";
+});
+
+cb1.addEventListener("click", function () {
+    if(cb1.checked)
+        document.getElementById('HeightCells').style.display = "block";
+    else
+        document.getElementById('HeightCells').style.display = "none";
+});
+
+cb2.addEventListener("click", function () {
+    if(cb2.checked)
+        document.getElementById('WeightCells').style.display = "block";
+    else
+        document.getElementById('WeightCells').style.display = "none";
+});
