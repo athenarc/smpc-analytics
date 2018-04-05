47923a7c5ed2761d85c029dfc82a907db5d2ffcd
Thanos Giannopoulos <thanosgn@gmail.com>
2018-04-05 11:10:49 +0300

    Fixed missing values in csv preprocessing

--- plot.py
+++ plot.py
@@ -68,7 +68,7 @@ with open('out.txt', 'r') as results:
                 filename =  'web/visuals/1D_Histogram'+'_'+str(os.getpid())+'_'+str(ai)+'.html'
                 plotly.offline.plot(figure, filename=filename, auto_open = False)
                 print('/'.join(filename.split('/')[2:]))
-            elif len(dimensions) == 2 and 1 not in dimensions:
+            elif (len(dimensions) == 2 and 1 not in dimensions) or len(dimensions) == 3 and 1 in dimensions:
                 y = dimensions[1]
                 sublists = [histogram[i:i+y] for i in xrange(0, len(histogram), y)]
                 trace = go.Heatmap(z=sublists)
