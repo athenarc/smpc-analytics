abe19552fad3bcc3568c797695d8f5abb0f1f121
Thanos Giannopoulos <thanosgn@gmail.com>
2018-04-05 13:38:39 +0300

    Added defauly value to checkboxes, if ticket

--- web/frontend/logic.js
+++ web/frontend/logic.js
@@ -3,22 +3,31 @@ var cb1 = document.querySelectorAll('input[type="checkbox"]')[1];
 var cb2 = document.querySelectorAll('input[type="checkbox"]')[2];
 
 cb0.addEventListener("click", function () {
-    if(cb0.checked)
+    if(cb0.checked){
         document.getElementById("AgeCells").style.display = "inline";
-    else
+        document.getElementById("AgeCells").value = "3";
+    } else {
         document.getElementById("AgeCells").style.display = "none";
+        document.getElementById("AgeCells").value = "";
+    }
 });
 
 cb1.addEventListener("click", function () {
-    if(cb1.checked)
+    if(cb1.checked){
         document.getElementById('HeightCells').style.display = "inline";
-    else
+        document.getElementById('HeightCells').value = "3";
+    } else {
         document.getElementById('HeightCells').style.display = "none";
+        document.getElementById('HeightCells').value = "";
+    }
 });
 
 cb2.addEventListener("click", function () {
-    if(cb2.checked)
+    if(cb2.checked){
         document.getElementById('WeightCells').style.display = "inline";
-    else
+        document.getElementById('WeightCells').value = "3";
+    } else {
         document.getElementById('WeightCells').style.display = "none";
+        document.getElementById('WeightCells').value = "";
+    }
 });
