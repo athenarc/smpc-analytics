c9d7637e4e3941abb10468a01271e0a3222d1389
Dimitris Mouris <jimouris@gmail.com>
2018-04-05 13:30:13 +0300

    Replace table with list

--- web/frontend/logic.js
+++ web/frontend/logic.js
@@ -4,21 +4,21 @@ var cb2 = document.querySelectorAll('input[type="checkbox"]')[2];
 
 cb0.addEventListener("click", function () {
     if(cb0.checked)
-        document.getElementById("AgeCells").style.display = "block";
+        document.getElementById("AgeCells").style.display = "inline";
     else
         document.getElementById("AgeCells").style.display = "none";
 });
 
 cb1.addEventListener("click", function () {
     if(cb1.checked)
-        document.getElementById('HeightCells').style.display = "block";
+        document.getElementById('HeightCells').style.display = "inline";
     else
         document.getElementById('HeightCells').style.display = "none";
 });
 
 cb2.addEventListener("click", function () {
     if(cb2.checked)
-        document.getElementById('WeightCells').style.display = "block";
+        document.getElementById('WeightCells').style.display = "inline";
     else
         document.getElementById('WeightCells').style.display = "none";
 });
