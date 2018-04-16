46f44bf9b04b2b867929f0139a1d49a0d0a0a239
Thanos Giannopoulos <thanosgn@gmail.com>
2018-04-16 13:48:52 +0300

    Generate main for histogram with filters

--- main_generator.py
+++ main_generator.py
@@ -19,8 +19,20 @@ import data_input;
 import histogram;
 '''
 main_f = '''void main(){
-    uint64 attributes_vmap = tdbVmapNew();'''
+'''
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def quote(x):
+    if is_number(x):
+        return str(x)
+    else:
+        return '"' + x + '"'
 
 def main():
     global main_f
@@ -35,18 +47,54 @@ def main():
     else:
         print('No arguement provided')
         sys.exit(1)
-    
+
     main_counter = configuration[14:-5]
-    
+
     number_of_histograms = 1
     configuration = json.load(open(configuration))
+    numberOfFilters = 0
+    if 'filter_attributes' in configuration:
+        if isinstance(configuration['filter_attributes'], list):
+            numberOfFilters = len(configuration['filter_attributes'])
+        else:
+            numberOfFilters = 1
     df=pd.read_csv(DATASET,sep=',')
     if isinstance(configuration['attributes'], list): # Multiple attributes
         attribute_indexes = [df.columns.get_loc(attribute) for attribute in configuration['attributes'] ]
     else: # Single attribute
         attribute_indexes = [df.columns.get_loc(configuration['attributes'])]
     attributes_vmap = '{'+ ', '.join([str(x) for x in attribute_indexes]) +'}'
+    if numberOfFilters > 0:
+        bool_op = quote(configuration['boolean_opreator'])
+        main_f += '''
+    string bool_op =''' + bool_op + ''';
+        '''
+        operators_enum = {'>' : 0, '<' : 1, '=': 2 }
+        if isinstance(configuration['filter_attributes'], list):
+            filter_attributes_indexes = [df.columns.get_loc(attribute) for attribute in configuration['filter_attributes'] ]
+            # main_f += '{'+ ', '.join([str(x) for x in attribute_indexes]) +'}'
+        else:
+            filter_attributes_indexes = [df.columns.get_loc(configuration['filter_attributes'])]
+        constraint_attributes = '{'+ ', '.join([str(x) for x in filter_attributes_indexes]) +'}'
+        main_f += '''
+    uint64[[1]] constraint_attributes = '''+ constraint_attributes + ';'
+        if isinstance(configuration['filter_operators'], list):
+            filter_operators_indexes = [operators_enum[x] for x in configuration['filter_operators']]
+            # main_f += '{'+ ', '.join([str(x) for x in attribute_indexes]) +'}'
+        else:
+            filter_operators_indexes = [operators_enum[configuration['filter_operators']]]
+        constraint_operators = '{'+ ', '.join([str(x) for x in filter_operators_indexes]) +'}'
+        main_f += '''
+    uint64[[1]] constraint_operators = '''+ constraint_operators + ';'
+        if isinstance(configuration['filter_values'], list):
+            constraint_values = '{'+ ', '.join([quote(x) for x in configuration['filter_values']]) +'}'
+        else:
+            constraint_values = '{'+ quote(configuration['filter_values']) +'}'
+        main_f += '''
+    float64[[1]] constraint_values = '''+ constraint_values + ';'
+
     main_f += '''
+    uint64 attributes_vmap = tdbVmapNew();
     uint64[[1]] vector_value;
     '''
     main_f +='''
@@ -61,8 +109,12 @@ def main():
     vector_value = ''' + cells_vmap + ';'
     main_f += '''
     tdbVmapAddValue(cells_vmap, "0", vector_value);
-    print("Computing histograms");
-    uint64 histograms = multiple_histograms(imported_array, '''+str(number_of_histograms)+'''::uint64, attributes_vmap, cells_vmap, imported_mins, imported_maxs);
+    print("Computing histograms");'''
+    if numberOfFilters > 0:
+        main_f += '''uint64 histograms = multiple_histograms(imported_array, '''+str(number_of_histograms)+'''::uint64, attributes_vmap, cells_vmap, imported_mins, imported_maxs, bool_op, constraint_attributes, constraint_operators, constraint_values);'''
+    else:
+        main_f += '''uint64 histograms = multiple_histograms(imported_array, '''+str(number_of_histograms)+'''::uint64, attributes_vmap, cells_vmap, imported_mins, imported_maxs);'''
+    main_f += '''
     pd_shared3p uint64[[1]] res;
     '''
     for i in range(number_of_histograms):
