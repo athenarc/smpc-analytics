3e35c68419fa9e03c99ac5fc476053547bfbccb5
Dimitris Mouris <jimouris@gmail.com>
2018-05-01 12:52:52 +0300

    Update clean script paths

--- sharemind-scripts/clean.sh
+++ sharemind-scripts/clean.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-rm -f ../.*.sb.src *.sb
-rm -f .././configuration_*.json
-rm -f .././histogram_main_*.sc
-rm -f .././out_*.txt
-rm -f .././web/visuals/*.html
+rm -f .*.sb.src *.sb
+rm -f ./histogram/.*.sb.src ./histogram/*.sb
+rm -f ./configuration_*.json
+rm -f ./histogram/histogram_main_*.sc
+rm -f ./out_*.txt
+rm -f ./*.out
+rm -f ./web/visuals/*.html
+rm -f ./web/graphs/id3_*.html
