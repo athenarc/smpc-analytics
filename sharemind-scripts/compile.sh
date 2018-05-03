c0da0ad79f205c4cbfef7dcbeff83391d7e65f8a
Dimitris Mouris <jimouris@gmail.com>
2018-05-03 11:09:24 +0300

    Remove previous compiled file in each compile

--- sharemind-scripts/compile.sh
+++ sharemind-scripts/compile.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 CC="sm_compile.sh"
 PWD=$(pwd)
+
+filepath=$(dirname "$1")
+filename=$(basename "$1" .sc)
+old="$filepath/.$filename.sb.src"
+echo "Deleting $old"
+
+rm -f $old
 ${CC} ${PWD}/$1
