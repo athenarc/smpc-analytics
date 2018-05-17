7271231b48434d76eff692a18d2e0be9566332b1
Thanos Giannopoulos <thanosgn@gmail.com>
2018-05-17 12:23:10 +0300

    Added mesh top level in path. Added depth function

--- dataset-scripts/mesh.py
+++ dataset-scripts/mesh.py
@@ -9,12 +9,15 @@ def construct_dict(csv_file, delimiter=';'):
         d = dict(reader)
         return d
 
+def mesh_tree_depth(code):
+    if len(code) == 1:
+        return 0
+    else:
+        return code.count('.') + 1
 
 def main():
     mesh_dict = construct_dict('../datasets/mtrees2018.csv') # name -> code
     mesh_dict_inverted = construct_dict('../datasets/mtrees2018_inverted.csv') # code -> name
-    # mesh_dict_inverted = dict((v, k) for k, v in mesh_dict.iteritems())
-    print(mesh_dict['Necrosis'])
 
     pateint_directory = '../datasets/patient_files'
     for filename in os.listdir(pateint_directory):
@@ -29,20 +32,18 @@ def main():
                         name = keyword['value']
                         code = mesh_dict[name]
                         mesh_branch = code.split('.')
-                        # print(name)
                         mesh_branch_codes = ['.'.join(mesh_branch[:i]) for i in range(1,len(mesh_branch)+1)]
-                        # print(mesh_branch_codes)
                         mesh_branch_names = [mesh_dict_inverted[code] for code in mesh_branch_codes]
+
                         top_level_code = mesh_branch_codes[0][0]
-                        # print(top_level_code)
-                        # print(mesh_dict_inverted[top_level_code])
+                        top_level_name = mesh_dict_inverted[top_level_code]
+                        mesh_branch_names = [top_level_name] + mesh_branch_names
                         print(' -> '.join(mesh_branch_names))
-                        print('Key: ' + mesh_branch_names[0] + ', Value: ' + name)
+                        # print('Key: ' + mesh_branch_names[0] + ', Value: ' + name)
                         print('\n')
 
 
         print('-----------------------------------------------------------------------------')
-    # print(len(mesh_dict_inverted.keys()))
 
 
 if __name__ == '__main__':
