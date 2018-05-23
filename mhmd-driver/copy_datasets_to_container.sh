29f7b6dbb368c6047a4c662d999ff3cf850f07af
Dimitris Mouris <jimouris@gmail.com>
2018-05-23 13:09:24 +0300

    Copy the MESH tree files in docker build

--- mhmd-driver/copy_datasets_to_container.sh
+++ mhmd-driver/copy_datasets_to_container.sh
@@ -3,6 +3,3 @@
 CONTAINER_NAME="$1"
 
 sudo docker cp ../datasets/patient_files/ $CONTAINER_NAME:/patient_files
-sudo docker cp ../datasets/mesh_mapping.json $CONTAINER_NAME:/mesh_mapping.json
-sudo docker cp ../datasets/mtrees2018.csv $CONTAINER_NAME:/mtrees2018.csv
-sudo docker cp ../datasets/mtrees2018_inverted.csv $CONTAINER_NAME:/mtrees2018_inverted.csv
