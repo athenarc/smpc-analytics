95f9cf97ea174ba2090529f322d77d9b18000acc
Thanos Giannopoulos <thanosgn@gmail.com>
2018-05-24 17:08:23 +0300

    Fix histogram/count output for multiple attributes

--- web/transform_response.py
+++ web/transform_response.py
@@ -2,6 +2,9 @@ import sys
 import json
 import argparse
 import operator
+import itertools
+import numpy
+from huepy import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('configuration', help= 'Configuration file of the request.')
@@ -12,16 +15,26 @@ with open (args.configuration) as configuration:
     configuration = json.loads(configuration.readline())
     line = json.loads(sys.stdin.readline())
     mesh_mapping = json.load(open(args.mapping))
-    attribute = configuration['attribute']
-    possible_values = mesh_mapping[attribute]
-    possible_values = [str(k) for k,v in sorted(possible_values.items(), key=operator.itemgetter(1))]
-    # cellsPerDimension = line[0]['cellsPerDimension'][0]
+    attributes = configuration['attributes']
+    possible_values = [mesh_mapping[attribute] for attribute in attributes]
+    possible_values = [[str(k) for k,v in sorted(values.items(), key=operator.itemgetter(1))] for values in possible_values]
+    cellsPerDimension = line[0]['cellsPerDimension']
     histogram = line[0]['histogram']
-    if len(possible_values) != len(histogram):
-        print('histogram length and possible_values do not match!')
+    if numpy.product(cellsPerDimension) != len(histogram):
+        print(bad('histogram length and possible_values do not match!'))
         sys.exit(1)
+    # for dimension in reversed(cellsPerDimension[1:]):
+    #     histogram = [histogram[i:i+dimension] for i in xrange(0, len(histogram), dimension)]
     output = {'data' : []}
-    for i in range(len(histogram)):
-        tuple = {'label' : possible_values[i], 'value' : histogram[i]}
-        output['data'].append(tuple)
+    i = 0
+    if len(possible_values) == 1:
+        for p in possible_values[0]:
+            tuple = {'label' : str(p), 'value' : histogram[i]}
+            output['data'].append(tuple)
+            i += 1
+    else:
+        for p in itertools.product(*possible_values):
+            tuple = {'label' : str(p), 'value' : histogram[i]}
+            output['data'].append(tuple)
+            i += 1
     print(json.dumps(output))
