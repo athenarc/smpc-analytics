6e09e60afe6f9058d80053837fba131e4874011b
Thanos Giannopoulos <thanosgn@gmail.com>
2018-05-29 15:44:18 +0300

    Edited script generating mesh mapping to include ids instead of names

--- dataset-scripts/possible_values.py
+++ dataset-scripts/possible_values.py
@@ -1,38 +1,40 @@
 from __future__ import print_function
-import csv
 import json
 import os
-import pandas
+import argparse
+from huepy import *
 
-OUTPUT_FILE = '../datasets/mesh_mapping.json'
+parser = argparse.ArgumentParser()
+parser.add_argument('--mtrees', help = 'File with the mesh term mapping (values to integers).', default = '/m.json')
+parser.add_argument('--mtrees_inverted', help = 'File with the mesh term mapping (values to integers).', default = '/m_inv.json')
+parser.add_argument('--output', help = 'The output csv to be created.', default = '../datasets/mesh_mapping.json')
+parser.add_argument('--verbose', help = 'See verbose output', action = 'store_true')
+args = parser.parse_args()
 
-def construct_dict(csv_file, delimiter=';'):
-    with open(csv_file) as f:
-        reader = csv.reader(f, skipinitialspace=True, delimiter=delimiter)
-        d = dict(reader)
-        return d
-
-def mesh_tree_depth(code):
-    if len(code) == 1:
+def mesh_tree_depth(id):
+    if len(id) == 1:
         return 0
     else:
-        return code.count('.') + 1
+        return id.count('.') + 1
 
 def main():
-    mesh_dict = construct_dict('../datasets/mtrees2018.csv') # name -> code
-    mesh_dict_inverted = construct_dict('../datasets/mtrees2018_inverted.csv') # code -> name
+    mesh_dict = json.load(open(args.mtrees))
+    mesh_dict_inverted = json.load(open(args.mtrees_inverted))
     direct_children = {}
 
-    for term in mesh_dict.keys():
-        code = mesh_dict[term]
-        depth = mesh_tree_depth(code)
-        children_codes = [key for key in mesh_dict_inverted.keys() if key.startswith(code) and mesh_tree_depth(key) == depth + 1]
-        childred_mapping = dict((mesh_dict_inverted[code] , i) for i,code in enumerate(children_codes) )
-        direct_children[term] = childred_mapping
-        # print(term+': --> '+str(childred_mapping))
+    print(run('Generating Mesh mapping..'))
+    for id in mesh_dict_inverted.keys():
+        depth = mesh_tree_depth(id)
+        children_ids = [key for key in mesh_dict_inverted.keys() if key.startswith(id) and mesh_tree_depth(key) == depth + 1]
+        childred_mapping = dict((id , i) for i,id in enumerate(children_ids) )
+        direct_children[id] = childred_mapping
+        if args.verbose:
+            print(info(id+': --> '+str(childred_mapping)))
 
-    with open(OUTPUT_FILE, 'w') as outfile:
+    with open(args.output, 'w') as outfile:
         json.dump(direct_children, outfile)
+
+    print(good('Mesh mapping generated successfully.'))
 
 if __name__ == '__main__':
     main()
