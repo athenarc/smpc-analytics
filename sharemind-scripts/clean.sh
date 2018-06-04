18211b8ad14ed7613c54f2f0b3593a698bff26d0
Thanos Giannopoulos <thanosgn@gmail.com>
2018-06-04 15:45:28 +0300

    Added files in cleaning script

--- sharemind-scripts/clean.sh
+++ sharemind-scripts/clean.sh
@@ -2,8 +2,11 @@
 rm -f .*.sb.src *.sb
 rm -f ./histogram/.*.sb.src ./histogram/*.sb
 rm -f ./configuration_*.json
-rm -f ./histogram/histogram_main_*.sc
+rm -f ./histogram/main*.sc
+rm -f ./ID3/main*.sc
+rm -f ./simulated_import_*.sc
 rm -f ./out_*.txt
+rm -f ./out_*.json
 rm -f ./*.out
 rm -f ./web/visuals/*.html
 rm -f ./web/graphs/id3_*.html
