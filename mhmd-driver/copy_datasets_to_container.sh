c865ebfab214648eede5736d0ecd64b9c05c6b6a
Dimitris Mouris <jimouris@gmail.com>
2018-06-11 11:16:09 +0300

    Import cvi with 20k patients

--- mhmd-driver/copy_datasets_to_container.sh
+++ mhmd-driver/copy_datasets_to_container.sh
@@ -3,4 +3,4 @@
 CONTAINER_NAME="$1"
 
 sudo docker cp ../datasets/patients.json $CONTAINER_NAME:/patients.json
-sudo docker cp ../datasets/analysis_test_data/cvi_identified.csv $CONTAINER_NAME:/cvi_identified.csv
+sudo docker cp ../datasets/analysis_test_data/cvi_identified_20k.csv $CONTAINER_NAME:/cvi_identified.csv
