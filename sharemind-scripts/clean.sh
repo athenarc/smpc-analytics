b0353f436fc20ea8d52d6670000f6e72f3a4c8af
Thanos Giannopoulos <thanosgn@gmail.com>
2018-06-13 12:12:02 +0300

    Added more files in clean script

--- sharemind-scripts/clean.sh
+++ sharemind-scripts/clean.sh
@@ -2,9 +2,10 @@
 rm -f .*.sb.src *.sb
 rm -f ./histogram/.*.sb.src ./histogram/*.sb
 rm -f ./configuration_*.json
-rm -f ./histogram/main*.sc
-rm -f ./ID3/main*.sc
-rm -f ./simulated_import_*.sc
+rm -f ./histogram/main*.s?
+rm -f ./ID3/main*.s?
+rm -f ./ID3/.*.sb.src
+rm -f ./simulated_import_*.s?
 rm -f ./out_*.txt
 rm -f ./out_*.json
 rm -f ./*.out
