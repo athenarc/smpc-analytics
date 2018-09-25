da22f1113302e115fa4d6f7daf5e65c681d0afff
Thanos Giannopoulos <thanosgn@gmail.com>
2018-09-25 16:53:45 +0300

    Combing ID3 main generators into one file

--- dataset-scripts/id3_main_generator.py
+++ dataset-scripts/id3_main_generator.py
@@ -4,6 +4,7 @@ import json
 from huepy import *
 import os.path
 import argparse
+import pandas as pd
 
 indentation = '    '
 
@@ -19,7 +20,9 @@ import shared3p_oblivious;
 
 import shared3p_table_database;
 import table_database;
-import id3_db_categorical;
+import id3_db;
+
+import create_possible_values;
 '''
 main_f = '''void main(){
 '''
@@ -43,16 +46,21 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('configuration', help = 'Configuration file of the request')
-    parser.add_argument('--mapping', help = 'File with the mesh term mapping (values to integers).', default = 'mhmd-driver/mesh_mapping.json')
-    parser.add_argument('--DNS', help = 'File with the Hospitals names and IPS.', default = 'web/MHMDdns.json')
+    parser.add_argument('--DNS', help = 'File with the Hospitals names and IPS.', default = './web/MHMDdns.json')
+    parser.add_argument('--summary', help = 'File with the Hospitals names and IPS.', default = './datasets/analysis_test_data/cvi_summary.csv')
+    parser.add_argument('--mesh_mapping', help = 'File with the mesh term mapping (values to integers).', default = 'mhmd-driver/mesh_mapping.json')
+    parser.add_argument('--cvi_mapping', help = 'File with the cvi categorical attributes mapping (values to integers).', default = 'datasets/analysis_test_data/cvi_mapping.json')
     args = parser.parse_args()
 
     print(run('Generating main..'))
 
     uid = args.configuration.split('_')[-1].split('.')[0]
     configuration = json.load(open(args.configuration))
-    mapping = json.load(open(args.mapping))
-
+    mesh_mapping = json.load(open(args.mesh_mapping))
+    cvi_mapping = json.load(open(args.cvi_mapping))
+    summary = pd.read_csv(args.summary, sep = ',')
+    
+    
     if 'datasources' in configuration:
         numberOfDatasets = len(configuration['datasources'])
         data_providers = '\n'.join([indentation + "string table_" + str(i) + " = " + quote(configuration['datasources'][i] + '_' + uid) + ";" for i in range(len(configuration['datasources']))])
@@ -61,17 +69,7 @@ def main():
         available_datasources = dns.keys()
         numberOfDatasets = len(available_datasources)
         data_providers = '\n'.join([indentation + "string table_" + str(i) + " = " + quote(available_datasources[i] + '_' + uid) + ";" for i in range(len(available_datasources))])
-
-    main_f += '''
-    quote = bl_str("\\"");
-    comma = bl_str(", ");
-    eq_str = bl_str(" == ");
-    space = bl_str(" ");
-    colon = bl_str(": ");
-    left_curly_br = bl_str("{ ");
-    right_curly_br = bl_str("}");
-
-'''
+    
     main_f += data_providers
     main_f += '''
     // Create the data-providers list
@@ -83,30 +81,74 @@ def main():
     tdbVmapAddString(providers_vmap, "0", table_'''+ str(i) +''');
 '''
     attributes = configuration['attributes']
-    class_attribute = configuration['class_attribute']
-    original_attributes = list(range(len(attributes)+1))
+    attribute_names = [str(a['name']) for a in attributes]
+    columns = len(attribute_names) + 1
+    total_attributes = attributes + [configuration['class_attribute']]
+
+    class_attribute = configuration['class_attribute']['name']
+    original_attributes = list(range(len(attribute_names)+1))
+    
     main_f += '''
     original_attributes = {'''+','.join(map(str,original_attributes))+'''};
-    pd_shared3p uint64[[1]] original_attributes_without_class = {'''+','.join(map(str,original_attributes[:-1]))+'''};
-    class_index = ''' + str(len(attributes)) + ''';
+    uint64[[1]] original_attributes_without_class = {'''+','.join(map(str,original_attributes[:-1]))+'''};
+    class_index = ''' + str(len(attribute_names)) + ''';
+    possible_values = tdbVmapNew();
+    float64[[1]] values;
 '''
 
-    attribute_values = [len(mapping[attribute]) for attribute in attributes]
-    possible_classes = list(range(len(mapping[class_attribute])))
-    max_attribute_values = max(max(attribute_values), len(possible_classes))
-    columns = len(attributes) + 1
-    # possible_values = [list(range(p)) for p in attribute_values]
-    possible_values = [possible_value + [-1]*(max_attribute_values-len(possible_value)) for possible_value in [list(range(p)) for p in attribute_values]]
-    possible_values.append(possible_classes + [-1]*(max_attribute_values-len(possible_classes))  )
+    imported_cells = [attribute['cells'] if 'cells' in attribute else 0 for attribute in total_attributes]
+
+    possible_values = {}
+    categorical_attributes = []
+    for counter, attribute in enumerate(total_attributes):
+        attribute_name = attribute['name']
+        if attribute_name in cvi_mapping:
+            possible_values[counter] = [mapping for value, mapping in cvi_mapping[attribute_name].items()]
+            categorical_attributes.append(counter)
+        elif attribute_name in mesh_mapping:
+            possible_values[counter] = list(range(len(mesh_mapping[attribute_name])))
+            categorical_attributes.append(counter)
+        elif 'cells' in attribute:
+            min = summary[summary['Field'] == attribute_name][' Min'].item()
+            max = summary[summary['Field'] == attribute_name][' Max'].item()
+            cells = int(attribute['cells'])
+            possible_values[counter] = list(range(cells))
+            categorical_attributes.append(-1)
+    
+    for attribute, values in possible_values.items():
+        main_f += '''
+    values = {''' + ','.join(map(quote, values)) + '''};
+    tdbVmapAddValue(possible_values, "''' + quote(attribute) + '''", values);
+ '''
+    
     main_f += '''
     columns = ''' + str(columns) + ''';
-    max_attribute_values = ''' + str(max_attribute_values) + ''';
-    possible_values = reshape({'''+','.join([','.join(map(str,possible_value)) for possible_value in possible_values])+'''},columns,max_attribute_values);
+    datasource = "DS1";
 '''
+
     main_f += '''
-    // Open connection to DB and Insert data to different tables
+    categorical_attributes = {''' + ','.join(map(quote,categorical_attributes)) + '''};
+ '''
+    
+    mins = []
+    maxs = []
+    for attribute in attribute_names+[class_attribute]:
+        if attribute in summary['Field'].values:
+            mins.append(summary[summary['Field']==attribute][' Min'].item())
+            maxs.append(summary[summary['Field']==attribute][' Max'].item())
+        else:
+            mins.append(0.0)
+            maxs.append(0.0)
+    main_f += '''
+    imported_mins =''' + '{'+ ', '.join([str(x) for x in mins]) +'}' + ''';
+    imported_maxs =''' + '{'+ ', '.join([str(x) for x in maxs]) +'}' + ''';
+
+    imported_cells =  {'''+','.join(map(str,imported_cells))+'''};
     print("Opening connection to db: ", datasource);
     tdbOpenConnection(datasource);
+'''
+
+    main_f += '''
 
     uint64 original_example_indexes_vmap = tdbVmapNew();
     for (uint64 i = 0 ; i < data_providers_num ; i++) {
@@ -117,11 +159,10 @@ def main():
         tdbVmapAddValue(original_example_indexes_vmap, "0", original_example_indexes);
     }
 '''
-
     main_f += '''
     print("Running ID3 ...");
-    pd_shared3p xor_uint8[[1]] root = id3(original_example_indexes_vmap, original_attributes_without_class);
-    print(bl_strDeclassify(root));
+    string root = id3(original_example_indexes_vmap, original_attributes_without_class);
+    print(root);
 
     for (uint64 i = 0 ; i < data_providers_num ; i++) {
         string table = tdbVmapGetString(providers_vmap, "0", i :: uint64);
@@ -134,6 +175,7 @@ def main():
     }
 }'''
 
+
     if os.path.isdir("./decision-tree/"):
         OUTPUT_DIR = './decision-tree/'
     elif os.path.isdir("../decision-tree/"):
@@ -144,7 +186,6 @@ def main():
         output.write(imports)
         output.write(main_f)
     print(good('Main generated at ' + OUTPUT_DIR + 'main_' + uid + '.sc'))
-
 
 
 if __name__ == '__main__':
